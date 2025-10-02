e52e8fb5c240e8406fd8a0995a1c7b723d756ae0
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-10-02 16:11:43 +0900

    practice6

--- main.c
+++ main.c
@@ -3,24 +3,31 @@
 
 int main(int argc, char *argv[])
 {
+    int answer = 59;
     int x;
-    int y;
-    char op;
+    int trial=0;
     
-    printf("Enter the calculation: ");
-    scanf("%i%c%i", &x, &op, &y);
     
-    if (op == '+')
-       printf("=%d\n", x+y);
-       
-    else if (op == '-')
-       printf("=%d\n", x-y);
-       
-    else if (op == '*')
-         printf("=%d\n", x*y);
-         
-    else if (op == '/')
-         printf("=%d\n", x/y);
+    
+    do
+    {
+        printf("Guess a number: ");
+        scanf("%i", &x);
+        
+        
+        if (x <answer)
+           printf("Low!\n");
+        else if (x>answer)
+             printf("high!\n");
+             
+        trial = trial+1;
+        
+        
+        
+    } while( x != answer); // x==answer°¡ ¾Æ´Ñ x!=answer... 
+    
+    printf("congratulation! trial: %d", trial);
+
          
    
     
